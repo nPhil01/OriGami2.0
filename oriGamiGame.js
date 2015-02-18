cd82ebf39afd78826a9e6ff4f1596a0c0d53d1fe
Marike Meijer <marikemeijer@googlemail.com>
2015-02-18 17:40:45 +0100

    Add ngGeolocation
    not working yet

--- oriGamiGame.js
+++ oriGamiGame.js
@@ -1,4 +1,4 @@
-var app = angular.module("oriGamiGame", ['leaflet-directive']);
+var app = angular.module("oriGamiGame", ['leaflet-directive'], ['ngGeolocation']);
 
 app.directive('navbar', function() {
   return {
@@ -18,3 +18,28 @@ app.controller("MapController", [ "$scope", function($scope) {
     },
   });
 }]);
+
+ /*
+  Geolocation Controller
+    get current position with HTML5 Geolocation
+  */
+app.controller('GeolocationController',['$geolocation', '$scope' function ($geolocation, $scope){
+  $scope.myPosition = $geolocation.getCurrentPosition({
+  timeout: 60000
+     }).then(function(position) {
+            $scope.myPosition = position;
+         });
+    }]);
+
+//watch position 
+
+app.controller('GeolocationController',['$geolocation', '$scope' function ($geolocation, $scope){
+   $geolocation.watchPosition({
+            timeout: 60000,
+            maximumAge: 250,
+            enableHighAccuracy: true
+        };
+        $scope.myCoords = $geolocation.position.coords; // this is regularly updated
+        $scope.myError = $geolocation.position.error; // this becomes truthy, and has 'code' and 'message' if an error occurs
+    }]);
+   );
