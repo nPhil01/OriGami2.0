4a8d4978d1c74a96f2f65cb391ab99dd3eca2114
zven <s.s.h@uni-muenster.de>
2015-03-04 13:38:38 +0100

    Remove commented code block (not used).

--- oriGamiGame.js
+++ oriGamiGame.js
@@ -120,6 +120,8 @@ app.controller("MapController", [ "$scope", function($scope, $http) {
 app.controller("GeoCtrl", function($scope, $window){
 
   $window.navigator.geolocation.watchPosition( function(position) {
+    //timeout: 60000,
+    //maximumAge: 250,
     $scope.position = position
     console.log(position.coords.latitude+' '+position.coords.longitude);
     // update map
@@ -143,29 +145,3 @@ app.controller("GeoCtrl", function($scope, $window){
   }
 
 });
-
-    /*
-    Geolocation Controller
-    get current position with HTML5 Geolocation
-    /*
-    app.controller('GeolocationController',['$geolocation', '$scope', function ($geolocation, $scope){
-    $scope.myPosition = $geolocation.getCurrentPosition({
-    timeout: 60000
-  }).then(function(position) {
-  $scope.myPosition = position;
-});
-}]);
-
-//watch position
-
-app.controller('GeolocationController',['$geolocation', '$scope' function ($geolocation, $scope){
-$geolocation.watchPosition({
-timeout: 60000,
-maximumAge: 250,
-enableHighAccuracy: true
-};
-$scope.myCoords = $geolocation.position.coords; // this is regularly updated
-$scope.myError = $geolocation.position.error; // this becomes truthy, and has 'code' and 'message' if an error occurs
-}]);
-);
-*/
