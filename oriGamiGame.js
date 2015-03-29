e0eae4b97c3825b59658da4a77c67695a819ce66
zven <s.s.h@uni-muenster.de>
2015-03-29 22:02:10 +0200

    Comments added.

--- oriGamiGame.js
+++ oriGamiGame.js
@@ -1,26 +1,33 @@
+/*
+The main part of the application, so far only in one javascript file.
+TODO: split in components
+*/
+
+// our app requires the leaflet and ui.bootstrap directives
 var app = angular.module("oriGamiGame", ['leaflet-directive','ui.bootstrap']);
 
 // self containing navbar directive
 app.directive('navbar', function() {
   return {
     scope: {
-      layers: '=bindlayers'
+      layers: '=bindlayers' // two way binding
     },
     restrict: 'E',
     templateUrl: "navbar.html",
     link: function(scope, elem, attrs) {
     },
+    // controller that connects the visibility of baselayers with selection in navbar
     controller: function($scope, $element){
       $scope.layerNames = [];
       // function that gets the layer names of layers object passed over to the directive
       $scope.setLayerNames = function () {
-        var names = [];
         for (var k in $scope.layers.baselayers) {
           $scope.layerNames[$scope.layerNames.length] = $scope.layers.baselayers[k].name
         }
       }
+      // call function to set the layer names
       $scope.setLayerNames();
-      // change visibility of layers based on passed layer name
+      // change visibility of layers based on passed layer name (function called on click on corresponding layer name in navbar)
       $scope.setLayer = function (layerName) {
         for (var k in $scope.layers.baselayers) {
           if ($scope.layers.baselayers[k].name==layerName) {
@@ -34,44 +41,51 @@ app.directive('navbar', function() {
   }
 });
 
+// custom directive for server connection
+// TODO: put in factory and module
 app.directive('server-connection', function() {
   return {
     restrict: 'E'
   }
 });
 
+// controller that provides function to load route from arcgis server
 app.controller("ServerConnectionController", [ "$scope", function($scope) {
   // use custom layer element from angular-leaflet-directive to load route
 
-  var routeID = "0GNo7";
+  var routeID = "0GNo7"; // hardcoded example
 
   $scope.loadRoute = function (routeID) {
     L.esri.featureLayer('http://giv-learn2.uni-muenster.de/arcgis/rest/services/GeoSpatialLearning/route/MapServer/0',
-    { fields: ['*'] })
+    { fields: ['*'] }) // query all metadata fields
     .query()
-    .where("route_id='"+routeID+"'")
-    .run(function(error, featureCollection){
-      console.log(featureCollection);
+    .where("route_id='"+routeID+"'") // filter by route id
+    .run(function(error, featureCollection){ // run the query
+      console.log(featureCollection); // so far only logging in console, TODO: use collection of waypoints in game
     });
   };
 
 }]);
 
+/*
+The main controller of the app
+*/
 app.controller("MapController", [ "$scope", function($scope, $http) {
   console.log("Create map controller");
+  // set the map properties
   angular.extend($scope, {
     // Center the map
     center: {
       autoDiscover: true,
       zoom: 8
     },
-    defaults: {
+    defaults: { // center when user denies us the right to access geolocation
       lat: 52,
       lng: 7,
       zoom: 6
     },
-    paths: {
-      userPos: {
+    paths: { // paths for drawing the circular marker(s) visualizing the user pos.
+      userPos: { // buffer around user position
         type: 'circleMarker',
         color: '#2E64FE',
         weight: 2,
@@ -80,7 +94,7 @@ app.controller("MapController", [ "$scope", function($scope, $http) {
         clickable : false,
         latlngs: {lat: 52, lng: 7}
       },
-      userPosCenter: {
+      userPosCenter: { // small blue dot at center of circle that denotes user position
         type: 'circleMarker',
         color: '#2E64FE',
         fill: true,
@@ -92,7 +106,7 @@ app.controller("MapController", [ "$scope", function($scope, $http) {
         latlngs: {lat: 52, lng: 7}
       }
     },
-    layers: {
+    layers: { // a set of different baselayers, more can be found here: http://leaflet-extras.github.io/leaflet-providers/preview/
       baselayers: {
         osm: {
           name: 'OpenStreetMap',
@@ -139,12 +153,14 @@ app.controller("MapController", [ "$scope", function($scope, $http) {
   });
 }]);
 
+// controller that takes care of obtaining the geo location
 app.controller("GeoCtrl", function($scope, $window){
 
+  // Function that watches the position and is called when it changes
   $window.navigator.geolocation.watchPosition( function(position) {
     //timeout: 60000,
     //maximumAge: 250,
-    $scope.position = position
+    $scope.position = position // set position in app
     console.log(position.coords.latitude+' '+position.coords.longitude+' '+position.coords.accuracy);
     // update map
     $scope.$apply(function(){
@@ -155,16 +171,18 @@ app.controller("GeoCtrl", function($scope, $window){
     });
   });
 
+  // function that updates the path objects (circle markers)
   $scope.updateMarker = function() {
     if ($scope.position != null) {
       $scope.paths.userPos.latlngs.lat = $scope.position.coords.latitude
       $scope.paths.userPos.latlngs.lng = $scope.position.coords.longitude
       $scope.paths.userPos.opacity = 1.0
-      $scope.paths.userPos.radius = $scope.metersToPixels($scope.position.coords.accuracy)
-      $scope.paths.userPos.updateTrigger = !$scope.paths.userPos.updateTrigger
+      $scope.paths.userPos.radius = $scope.metersToPixels($scope.position.coords.accuracy) // update radius with accuracy of position
+      $scope.paths.userPos.updateTrigger = !$scope.paths.userPos.updateTrigger // change value of update trigger that is watched below
     }
   }
 
+  // function that converts meters to pixels for the current zoom level
   $scope.metersToPixels = function(meters) {
     // http://wiki.openstreetmap.org/wiki/Zoom_levels
     // S=C*cos(y)/2^(z+8)
@@ -177,6 +195,7 @@ app.controller("GeoCtrl", function($scope, $window){
     return (meters/distOnePixelInMeters)
   }
 
+  // watch the current zoom level to update the size of accuracy based on zoom level
   $scope.$watch("center.zoom", function(zoom) {
     $scope.updateMarker();
   });
