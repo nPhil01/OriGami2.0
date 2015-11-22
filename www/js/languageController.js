818d467e7679753317664d2f98bde78f265fb96e
AlexIsHappy <alexishappy@yandex.ru>
2015-11-22 16:34:19 +0100

    Languages Support

--- www/js/languageController.js
+++ www/js/languageController.js
@@ -1,7 +1,28 @@
-angular.module('starter').controller('languageController',[ '$scope','$ionicModal','$translate',
-   function($scope,$ionicModal,$translate
-      ) {
-       $scope.changeLanguage = function(language){
-		  $translate.use(language);
+angular.module('starter').controller('languageController', ['$scope', '$ionicModal', '$translate', '$timeout',
+   function ($scope, $ionicModal, $translate, $timeout) {
+        $scope.currentLanguage = "English";
+        $scope.icon = 'question_answer';
+       
+        $scope.changeLanguage = function (language) {
+            $translate.use(language);
+
+            if (language == "prt")
+                $scope.currentLanguage = "Portuguese";
+             if (language == "esp")
+                $scope.currentLanguage = "Spanish";
+             if (language == "cat")
+                $scope.currentLanguage = "Catalan";
+             if (language == "de")
+                $scope.currentLanguage = "Deutsch";
+            if (language == "en")
+                $scope.currentLanguage = "English";
+            
+             $scope.icon = 'thumb_up';
+            
+            $timeout(function() {
+            $scope.icon = 'question_answer';
+           }, 1200);
         };
+
+
    }]);
