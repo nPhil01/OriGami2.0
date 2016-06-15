d81e3e6dda9a9f7c5d5ab2751664b6152afc054b
Mohammed Imaduddin Humayun <md.imad@gmail.com>
2016-06-15 14:17:49 +0200

    add code for running mongodb rest server

---
+++ mongodb_rest_server/server.js
@@ -0,0 +1,87 @@
+var restify = require('restify');
+var mongojs = require('mongojs');
+var db = mongojs('origami', ['games']);
+
+var server = restify.createServer();
+server.use(
+  function crossOrigin(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+);
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.listen(8000, function () {
+  console.log("Server started");
+});
+
+// Get only one certain game
+server.get("/games/item/:name", function (req, res, next) {
+  db.games.find({ "name": req.params.name }, function (err, games) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+    res.end(JSON.stringify(games));
+  });
+  return next();
+});
+
+//Get all the games
+server.get("/games", function (req, res, next) {
+  db.games.find(function (err, games) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json;charset=utf-8'
+    });
+    res.end(JSON.stringify(games));
+  });
+
+  return next();
+});
+
+// Add new game to the list
+server.post("/games/item", function (req, res, next) {
+  var item = req.params;
+  db.games.save(item, function (err, data) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+    res.end(JSON.stringify(data));
+  });
+  return next();
+});
+
+// Delete certain game
+server.del("/games/item/:name", function (req, res, next) {
+  db.games.remove({ 'name': req.params.name }, function (err, data) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+    res.end(JSON.stringify(data));
+  });
+  return next();
+});
+
+
+server.del("games/item/:id", function (req, res, next) {
+  db.games.remove({ 'name': req.params.name }, function (err, data) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json; charset=utf-8'
+    });
+    res.end(JSON.stringify(data));
+  });
+  return next();
+});
+
+// Get only game metadata from the database - getting all games was shown to be slow
+server.get("/games/metadata", function (req, res, next) {
+  db.games.find({}, { name: 1, description: 1, timecompl: 1, difficulty: 1 }, function (err, data) {
+    res.writeHead(200, {
+      'Content-Type': 'application/json;charset=utf-8'
+    });
+    res.end(JSON.stringify(data));
+  });
+  return next();
+});
