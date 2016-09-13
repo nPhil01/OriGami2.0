f9a07ae6fa0c9b176b223f0aa0772bec99b2ca80
Matthias Pfeil <matthias.pfeil@wwu.de>
2016-09-13 11:23:49 +0200

    setup dev task

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,32 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var watch = require('gulp-watch');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var del = require('del');
 
-gulp.task('webserver', ['copy', 'watch'], function() {
-  gulp.src('dist')
+gulp.task('clean', function (cb) {
+  del(['dist']);
+  cb();
+});
+
+gulp.task('webserver', function () {
+  var stream = gulp.src('dist')
     .pipe(webserver({
       livereload: true,
       open: true
     }));
+  return stream;
 });
 
-gulp.task('watch', function() {
-  gulp.watch('www/**/*', ['copy']);
-});
-
-gulp.task('copy', function() {
-  gulp.src('www/**/*')
+gulp.task('watch', function () {
+  return gulp.src('www/**/*')
+    .pipe(watch('www/**/*'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+gulp.task('copy', function (cb) {
+  gulp.src('www/**/*')
+    .pipe(gulp.dest('dist'));
+  cb();
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
+gulp.task('dev', ['clean', 'copy', 'watch', 'webserver']);
