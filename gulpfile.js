13c7168658118e3e833b94088b7162b9fadf5a98
Matthias Pfeil <matthias.pfeil@wwu.de>
2016-09-14 11:30:33 +0200

    install npm dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var watch = require('gulp-watch');
+var inject = require('gulp-inject');
 var del = require('del');
+var packageJson = require('./package.json');
+
+gulp.task('copy_packages', function () {
+  var modules = Object.keys(packageJson.dependencies);
+  var moduleFiles = modules.map(function(module) {
+    return 'node_modules/' + module + '/**/*.*';
+  });
+  return gulp.src(moduleFiles, { base: 'node_modules' })
+    .pipe(gulp.dest('dist/assets'));
+});
 
 gulp.task('clean', function (cb) {
   del(['dist']);
@@ -29,4 +40,4 @@ gulp.task('copy', function (cb) {
   cb();
 });
 
-gulp.task('dev', ['clean', 'copy', 'watch', 'webserver']);
+gulp.task('dev', ['clean', 'copy', 'copy_packages', 'watch', 'webserver']);
