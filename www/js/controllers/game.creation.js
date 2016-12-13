7017fa38d1a0c6b6df0a70ddd9df0f2d411f945f
Matthias Pfeil <matthias.pfeil@wwu.de>
2016-12-13 14:09:40 +0100

    all slides back

--- www/js/controllers/game.creation.js
+++ www/js/controllers/game.creation.js
@@ -23,7 +23,10 @@
     	vm.rateGame = rateGame;
     	vm.initSlideBox = initSlideBox;
 		vm.submit = submit;
+		vm.currentAct = {}; // Activity that is currently created
 		vm.chooseActType = chooseActType;
+		vm.finishGame = finishGame;
+		vm.slideTitle = 'General Information';
 
 		activate();
 
@@ -46,11 +49,32 @@
 		}
 
 		function nextSlide () {
+			if ($ionicSlideBoxDelegate.currentIndex() === 1) {
+				addActivity();
+			}
 			$ionicSlideBoxDelegate.next();
 		}
 
 		function slideChanged (slideIndex) {
 			console.log('slide changed');
+			console.log(vm.newgame)
+			if (slideIndex === 0) {
+				vm.slideTitle = 'General Information';
+			}
+			if (slideIndex === 1) {
+				vm.slideTitle = 'Choose Activity Type';
+				if (vm.act_type === 0) {
+					submit(true);
+				} else {
+					submit(false);
+				}
+			}
+			if (slideIndex === 2) {
+				vm.slideTitle = 'Add points to the map';
+			}
+			if (slideIndex === 3) {
+				vm.slideTitle = 'Congratulation!';	
+			}
 		}
 
 		function showSlideButtons () {
@@ -62,12 +86,16 @@
 
 			if (currentIndex === 0) {
 				showPrevious = false;
-			} else if (currentIndex > 0 && currentIndex < slidesCount - 1) {
+			} else if (currentIndex > 0 && currentIndex < slidesCount - 2) {
 				showPrevious = true;
-			} else if (currentIndex === slidesCount - 1) {
+			} else if (currentIndex === slidesCount - 2) {
 				showPrevious = true;
 				showNext = false;
 				saveButton = true;
+			} else if (currentIndex === slidesCount - 1) {
+				showPrevious = false;
+				showNext = false;
+				saveButton = false;
 			}
 
 			return { 
@@ -93,11 +121,37 @@
 
 		//Choose Activity
 		function chooseActType (type) {
-		    if (type === vm.act_type)
+		    if (type === vm.act_type) {
 		        vm.act_type = 0;
-		    else {
+		    	submit(true);
+		    } else {
 		        vm.act_type = type;
+		        submit(false);
 		    }
-		};
+		}
+
+	    function addActivity () {
+	        vm.newgame.activities = [];
+	        vm.currentAct.type = vm.act_type == 1 ? "Find destination" : "Follow route";
+	        vm.currentAct.points = [];
+	    }
+
+	    function finishGame () {
+	    	nextSlide();
+	        // API.saveItem(vm.newgame)
+	        //     .success(function (data, status, headers, config) {
+	        //         // $rootScope.hide();
+	        //         // $rootScope.doRefresh(1);
+	        //         // $ionicHistory.goBack();
+	        //         // $scope.newgame = {};
+	        //     })
+	        //     .error(function (data, status, headers, config) {
+	        //         // $rootScope.hide();
+	        //         // $rootScope.notify("Oops something went wrong!! Please try again later");
+	        //         // $ionicHistory.goBack();
+	        //         // $scope.newgame = {};
+	        //         // $scope.numberTask = 0;
+	        //     });
+	    }
 	}
 })();
