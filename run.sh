122f45181ccc074ab339580c7042ba24f6f78f13
Matthias Pfeil <matthias.pfeil@wwu.de>
2017-01-20 12:06:08 +0100

    fix Dockerfile and run npm an dbower install

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ IFS=$'\n\t'
 
 api_adr=${API_URL:-https://giv-origami.uni-muenster.de/origamidb}
 
-sed -i -e "s|ORIGAMI_API_URL|$api_adr|g" /usr/src/app/www/js/services.js
+sed -i -e "s|ORIGAMI_API_URL|$api_adr|g" /usr/src/dist/js/services.js
 
 exec /bin/true
