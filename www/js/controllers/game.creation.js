3479a7bb7974f7b03e4d089acfd9bf91b047a0ec
Matthias Pfeil <matthias.pfeil@wwu.de>
2017-02-03 15:46:27 +0100

    number waypoint markers during game creation

--- www/js/controllers/game.creation.js
+++ www/js/controllers/game.creation.js
@@ -186,5 +186,93 @@
 	            $scope.modal.show();
 	        });
 	    };
+
+	    var newMarker = {};
+	    $scope.numberTask = 0;
+	    $scope.saveWayPoint = function () {
+	        if (($scope.newWaypoint.name == "" || $scope.newWaypoint.name == undefined) || ($scope.newWaypoint.description == undefined || $scope.newWaypoint.description == "")) {
+
+	            if ($scope.newWaypoint.name == "" || $scope.newWaypoint.name == undefined) {
+	                $scope.name_border = "red";
+	            } else {
+	                $scope.name_border = "";
+	            }
+
+	            if ($scope.newWaypoint.description == undefined || $scope.newWaypoint.description == "") {
+	                $scope.description_border = "red";
+	            } else {
+	                $scope.description_border = "";
+	            }
+	        } else {
+	            $scope.name_border = "";
+	            $scope.description_border = "";
+
+	            newMarker = $scope.newWaypoint;
+	            newMarker.icon = angular.copy(waypointIcon);
+	            newMarker.icon.number = $scope.mainMap.markers.length + 1;
+	            $scope.mainMap.markers.push($scope.newWaypoint);
+
+	            $scope.closeModal();
+	            createModal('templates/tasks/task_choose.html', 'm2');
+	        }
+	    };
+
+	    var waypointIcon = {
+			type: 'extraMarker',
+			icon: 'fa-number',
+			markerColor: 'blue',
+			shape: 'circle',
+			number: '1'
+	    }
+
+	    $scope.mainMap = {
+	        center: {
+	            autoDiscover: true,
+	            zoom: 16
+	        },
+	        defaults: {
+	            tileLayer: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+	            maxZoom: 18,
+	            zoomControlPosition: 'topleft',
+	            lat: 57,
+	            lng: 8
+	        },
+	        markers: [],
+	        events: {},
+	        layers: {
+	            baselayers: {
+	                osm: {
+	                    name: 'Satelite View',
+	                    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+	                    type: 'xyz',
+	                    top: true,
+	                    layerOptions: {
+	                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	                        continuousWorld: false
+	                    }
+	                },
+	                streets: {
+	                    name: 'Streets View',
+	                    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+	                    type: 'xyz',
+	                    top: false,
+	                },
+	                topographic: {
+	                    name: 'Topographic View',
+	                    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+	                    type: 'xyz',
+	                    top: false,
+	                    layerOptions: {
+	                        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
+	                        continuousWorld: false
+	                    }
+	                }
+	            }
+	        }
+	    };
+
+		$scope.closeModal = function () {
+			$scope.modal.remove();
+		};
 	}
 })();
